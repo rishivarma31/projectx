5bb6fa3d0c983df943aca058ab8c80b7df1140a9
rishi <rishi@LAPTOP-A3OA0TIU>
2024-05-14 19:22:25 +0530

    program for printing even numbers

---
+++ src/projectx/File2.java
@@ -0,0 +1,19 @@
+package projectx;
+
+public class File2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		System.out.println(" even numbers from 1 to "+n+":");
+		for(int i=1;i<=n;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
